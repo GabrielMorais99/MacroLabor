e37f6fc49bc0fc80240d37481ff8b22851c4c6e2
GabrielMorais99 <gabrielmorais80@hotmail.com>
2020-07-08 23:55:20 -0300

    comentários das alterações

---
+++ MacroLabor/API/Repositório/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repositório
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
